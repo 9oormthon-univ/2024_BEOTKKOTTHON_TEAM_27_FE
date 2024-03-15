c42791a094c07d46daed8854e4754a79d0eac841
imeureka <lky062@naver.com>
2024-03-15 11:50:21 +0900

    ✔️ code review : warning 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,11 @@
 import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider, QueryErrorResetBoundary } from '@tanstack/react-query';
 import router from './router/Router';
-import ReactDOM from 'react-dom/client';
 import { ErrorBoundary } from 'react-error-boundary';
 import FallbackUI from './pages/FallbackUI/FallbackUI';
-import React, { Suspense } from 'react';
+import { Suspense } from 'react';
 import Loading from './pages/Loading/Loading';
 import GlobalStyle from './style/GlobalStyle';
-
-async function enableMocking() {
-  if (process.env.NODE_ENV !== 'development') {
-    return;
-  }
-
-  const { worker } = await import('./mocks/browser');
-  return worker.start();
-}
-
-enableMocking().then(() => {
-  const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  );
-});
 
 function App() {
   const queryClient = new QueryClient();
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,19 @@ import App from './App.tsx';
 import './index.css';
 import Styles from './style/index.tsx';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Styles>
-    <App />
-  </Styles>,
-);
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
+
+  const { worker } = await import('./mocks/browser');
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <Styles>
+      <App />
+    </Styles>,
+  );
+});
