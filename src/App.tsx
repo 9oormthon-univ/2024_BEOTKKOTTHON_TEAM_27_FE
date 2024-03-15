a337167c196c3bcf524bac8441cfd3bfbfbc88a3
imeureka <lky062@naver.com>
2024-03-15 11:14:18 +0900

    ✨ feat : msw 적용 및 테스트 확인

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './router/Router';
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
+
+  const { worker } = await import('./mocks/browser');
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+});
 
 function App() {
   return (
