a493339f2dc7e234b29c2dfdaaed632fd06fdf16
imeureka <lky062@naver.com>
2024-03-15 15:07:36 +0900

    ✔️ code review : 크롬 익스텐션 warning 해결

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,9 @@ async function enableMocking() {
   }
 
   const { worker } = await import('./mocks/browser');
-  return worker.start();
+  return worker.start({
+    onUnhandledRequest: 'bypass',
+  });
 }
 
 enableMocking().then(() => {
