717ec10abf5445d6f4a298d4827c028baf6f7889
imeureka <lky062@naver.com>
2024-03-16 23:18:57 +0900

    🚨 fix : 빌드 에러 해결

--- src/assets/svg/Ai.tsx
+++ src/assets/svg/Ai.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import type { SVGProps } from 'react';
 const SvgAi = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 25 16' {...props}>
