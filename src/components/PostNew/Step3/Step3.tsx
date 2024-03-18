3c7d6173dfc1b469e9adf881b9f220f3bf82cbd3
imeureka <lky062@naver.com>
2024-03-19 00:17:49 +0900

    ✨ feat : step3 퍼블리싱

---
+++ src/components/PostNew/Step3/Step3.tsx
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+import PostFooter from '../PostFooter/PostFooter';
+import SelectType from './SelectType/SelectType';
+
+export default function Step3() {
+  return (
+    <>
+      <PostTitleContainer>
+        <Title>
+          <Highlight>홍보 목적</Highlight>을 <br />
+          선택해 주세요
+        </Title>
+        <SubTitle>
+          각 홍보에 목적에 따라 <br /> 다른 형식의 포스팅이 작성됩니다!
+        </SubTitle>
+      </PostTitleContainer>
+
+      <SelectType />
+      <PostFooter />
+    </>
+  );
+}
+
+const PostTitleContainer = styled.div`
+  margin-top: 7.5rem;
+  width: 100vw;
+`;
+
+const Title = styled.p`
+  ${({ theme }) => theme.fonts.PostingTitle};
+`;
+
+const Highlight = styled.span`
+  color: ${({ theme }) => theme.colors.main};
+`;
+
+const SubTitle = styled.p`
+  ${({ theme }) => theme.fonts.subTitle};
+`;
--- src/pages/PostNew/PostNew.tsx
+++ src/pages/PostNew/PostNew.tsx
@@ -1,16 +1,19 @@
 import { styled } from 'styled-components';
 // import Step2 from '../../components/PostNew/Step2/Step2';
-import Step1 from '../../components/PostNew/Step1/Step1';
+// import Step1 from '../../components/PostNew/Step1/Step1';
+import Step3 from '../../components/PostNew/Step3/Step3';
 
 export default function PostNew() {
   return (
     <PostNewContainer>
-      <Step1 />
-      {/* <Step2 /> */}
+      {/* <Step1 />
+      <Step2 /> */}
+      <Step3 />
     </PostNewContainer>
   );
 }
 
 const PostNewContainer = styled.section`
   padding: 0 1.5rem;
+  width: 100vw;
 `;
