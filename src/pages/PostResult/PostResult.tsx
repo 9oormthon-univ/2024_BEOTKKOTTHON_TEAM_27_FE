aadd5bb7834eac0efe70cbef8ce1e8522a332d30
Yooshin Kim <yusinkim.or@gmail.com>
2024-03-20 16:26:30 +0900

    ✨ feat : react-spring-bottom-sheet 설치 및 테스트

--- src/pages/PostResult/PostResult.tsx
+++ src/pages/PostResult/PostResult.tsx
@@ -5,6 +5,10 @@ import PostText from '../../components/PostResult/PostText';
 import PostButton from '../../components/PostResult/PostButton';
 import { useWindowSize } from '../../hooks/useWindowSize';
 import Confetti from 'react-confetti';
+import { useState } from 'react';
+import { BottomSheet } from 'react-spring-bottom-sheet';
+import 'react-spring-bottom-sheet/dist/style.css';
+import { Image, Text } from '../../assets/svg';
 
 export default function PostResult() {
   const post = {
@@ -15,6 +19,7 @@ export default function PostResult() {
   };
 
   const { width, height } = useWindowSize();
+  const [open, setIsOpen] = useState(true);
 
   return (
     <PostResultContainter>
@@ -23,6 +28,25 @@ export default function PostResult() {
       <PostText text={post.text} />
       <PostButton image={post.image} text={post.text} sns={post.sns} />
       <Confetti width={width} height={height} recycle={false} />
+      <BottomSheet open={open}>
+        <BottomSheetContainer>
+          <BottomSheetTitle>포스트 다시 생성하기</BottomSheetTitle>
+          <BottomSheetSubtitle>
+            다시 생성하기는 <b>15초</b> 이후, 최대 <b>2회</b>까지 가능해요
+          </BottomSheetSubtitle>
+
+          <BottomSheetSelectContainer>
+            <BottomSheetSelect>
+              <Image style={{ width: '110px' }} />
+              <p>이미지 생성하기</p> <div>0/3</div>
+            </BottomSheetSelect>
+            <BottomSheetSelect>
+              <Text style={{ width: '110px' }} />
+              <p>글 생성하기</p> <div>3/3</div>
+            </BottomSheetSelect>
+          </BottomSheetSelectContainer>
+        </BottomSheetContainer>
+      </BottomSheet>
     </PostResultContainter>
   );
 }
@@ -39,4 +63,55 @@ const PostResultContainter = styled.div`
   width: 100%;
   height: 100%;
   padding-bottom: calc(3.125rem + 3.125rem + 1rem + 1rem); // 버튼 영역 고려
+`;
+
+const BottomSheetContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+
+  padding: 1.5rem 1.7rem;
+`;
+
+const BottomSheetTitle = styled.h3`
+  ${({ theme }) => theme.fonts.heading_02};
+`;
+
+const BottomSheetSubtitle = styled.p`
+  color: ${({ theme }) => theme.colors.gray};
+  ${({ theme }) => theme.fonts.p_01};
+
+  > b {
+    font-weight: 700;
+  }
+`;
+
+const BottomSheetSelectContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  width: 100%;
+  margin-top: 1.75rem;
+`;
+
+const BottomSheetSelect = styled.div`
+  flex: 1;
+  cursor: pointer;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  color: ${({ theme }) => theme.colors.deep_gray};
+
+  > img {
+  }
+
+  > p {
+    ${({ theme }) => theme.fonts.content_01};
+  }
+
+  > div {
+    ${({ theme }) => theme.fonts.ex_01};
+  }
 `;
