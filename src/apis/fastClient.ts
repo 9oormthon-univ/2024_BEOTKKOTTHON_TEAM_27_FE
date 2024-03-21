a9301b86f64bab241528be1bf1c281e6a39bf0ba
Yooshin Kim <yusinkim.or@gmail.com>
2024-03-22 02:45:24 +0900

    📦 chore : 이미지 API 인스턴스 분리 -> fastClient

---
+++ src/apis/fastClient.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+// [이미지 업로드]
+// 8002 포트번호 다른 이유로 인스턴스 생성
+export const imageinstance = axios.create({
+  baseURL: import.meta.env.VITE_APP_IMG_URL,
+  withCredentials: false,
+  headers: {},
+});
+
+export function put<T>(...args: Parameters<typeof imageinstance.put>) {
+  return imageinstance.put<T>(...args);
+}
--- src/components/PostNew/Step6/Step6.tsx
+++ src/components/PostNew/Step6/Step6.tsx
@@ -5,9 +5,9 @@ import food1 from '../../../assets/Image/food1.jpg';
 import food2 from '../../../assets/Image/food2.jpg';
 import food3 from '../../../assets/Image/food3.jpg';
 import { IcEmptyThumbnailFinal, TipBtn } from '../../../assets/svg';
-import { put } from '../../../apis/client';
 import { useOnboardingContext } from '../../../context/PostNew/PostNewContext';
 import usePostOnboardingInfo from '../../../queries/PostNew/usePostInfo';
+import { put } from '../../../apis/fastClient';
 
 interface ServerResponse {
   file_name: string;
