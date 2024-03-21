8785ab95b21ecb41e0947247f09d7f4bf16e0d60
imeureka <lky062@naver.com>
2024-03-21 12:55:35 +0900

    ✨ feat : STEP 3 context 관리

--- src/components/PostNew/Step3/SelectType/SelectType.tsx
+++ src/components/PostNew/Step3/SelectType/SelectType.tsx
@@ -1,12 +1,17 @@
 import { useState } from 'react';
 import styled from 'styled-components';
 import { GenderOptionProps } from '../../../../types/PostNew';
+import { useOnboardingContext } from '../../../../context/PostNew/PonstNewContext';
 
 export default function SelectType() {
-  const [selectedType, setSelectedType] = useState<string | null>(null);
+  const { onboardingInfo, updatePostInfo } = useOnboardingContext();
+  const [selectedType, setSelectedType] = useState<string | null>(
+    onboardingInfo.promotionType || null,
+  );
 
   function handleGenderSelect(type: string) {
     setSelectedType(type);
+    updatePostInfo({ promotionType: type });
   }
 
   const typeOptions = [
@@ -60,7 +65,6 @@ const TypeOption = styled.div<GenderOptionProps>`
   background: #fff;
   box-shadow: 1px 1px 13px 1px rgba(0, 0, 0, 0.07);
 
-  ${({ theme }) => theme.fonts.heading_01};
-  border: ${({ selected }) => (selected ? '1px solid #402FFF' : 'none')};
-  color: ${({ selected }) => (selected ? '#402FFF' : '#9D9D9D')};
+  ${({ theme, selected }) =>
+    selected ? `border: 1px solid ${theme.colors.main}; color: ${theme.colors.main}; ` : ''}
 `;
