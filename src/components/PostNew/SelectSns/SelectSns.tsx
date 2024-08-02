13243f4621a6d4197e761beffedd5f6dfe80754d
lky062@naver.com <lky062@naver.com>
2024-08-02 17:27:34 +0900

    chore : 중복 삭제

--- src/components/PostNew/SelectSns/SelectSns.tsx
+++ src/components/PostNew/SelectSns/SelectSns.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
 import { CheckIcon } from '../../../assets/svg';
 import kakao from '../../../assets/Image/kakao.png';
